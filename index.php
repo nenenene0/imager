9c8692d8778a253485babc0e921b03e03b2d6705
neinderthal <bhaswanth.u@gmail.com>
2014-09-29 19:47:09 +0530

    maxImages variable added

--- index.php
+++ index.php
@@ -1,7 +1,13 @@
 <?php
-	$dir = scandir(dirname(__FILE__));
+	// Max # images per page
+	$maxImages = 30;
 	
+	// Getting the images in the current folder of index.php
+	$dir = scandir(dirname(__FILE__));
+	// Removing . and .. (DirectoryIterator::isDot can also be used)
+	// Best would be array_diff with '.', '..', '.htaccess', '.svn', 'index.php' [Issue will be raised]
 	$dir= array_slice($dir, 3);
+	
 	foreach ($dir as $fileinfo) {
 	        // we have each image name here.
 	        echo '<img src="'.$fileinfo.'" style="width:100%;">';
